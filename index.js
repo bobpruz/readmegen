60d84891a8fe10f3e4a8c1e4a6a49b7f58899e35
bobpruz <bobpruz@gmail.com>
2022-04-09 07:53:06 -0500

    add writeFile add init function

--- index.js
+++ index.js
@@ -95,10 +95,22 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const readmeText = generateMarkdown(data);
+
+    fs.writeFile(`./dist/${fileName}`, readmeText, err => {
+        if (err) throw err;
+        console.log(`${fileName} is generated.`);
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+        writeToFile('README.md', answers);
+    });
+}
 
 // Function call to initialize app
 init();
