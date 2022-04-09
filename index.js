f07054fc1cd19f470c0b0c74fd6baa8bd85a9511
bobpruz <bobpruz@gmail.com>
2022-04-09 07:41:30 -0500

    questions added

--- index.js
+++ index.js
@@ -4,7 +4,95 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter the name of your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description of your project',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please write a description of your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'website',
+        message: 'Enter the website link',
+        validate: websiteInput => {
+            if (websiteInput) {
+                return true;
+            } else {
+                console.log('Please enter the website link!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'maintainers',
+        message: 'Enter the name of the Maintainer',
+        validate: maintainersInput => {
+            if (maintainersInput) {
+                return true;
+            } else {
+                console.log('Please enter the name of the Maintainer!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter email address of the maintainer',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter email address of the maintainer!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: 'Enter the screenshot filename',
+        validate: screenshotInput => {
+            if (screenshotInput) {
+                return true;
+            } else {
+                console.log('Please enter the screenshot filename!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license does your project have?',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3',
+                 'Mozilla Public License 2.0', 'Apache License 2.0',
+                 'MIT License', 'Boost Software License 1.0', 'None']
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
